3a491fa615341678a76c30f195dd2f2bcf7511d9
Cythun <sdohri21@wooster.edu>
2019-01-15 15:58:43 -0500

    answered the exercism robot name exercise

--- exercism/python/robot-name/robot_name.py
+++ exercism/python/robot-name/robot_name.py
@@ -1,3 +1,33 @@
+import random
+
 class Robot(object):
     def __init__(self):
-        pass
+        chars = ('A', 'B', 'C', 'D', 'E', 'F', 'F', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+
+        nums = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
+
+        output = ''
+        for i in range(2):
+            output += random.SystemRandom().choice(chars)
+
+        for i in range(3):
+            output += random.SystemRandom().choice(nums)
+
+        self.name = output
+
+    def reset(self):
+        chars = ('A', 'B', 'C', 'D', 'E', 'F', 'F', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+
+        nums = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
+
+        output = ''
+        for i in range(2):
+            output += random.SystemRandom().choice(chars)
+
+        for i in range(3):
+            output += random.SystemRandom().choice(nums)
+
+        self.name = output
+
