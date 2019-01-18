9f6ec1ef8443bd6578f68bc3ad006cace052e723
Cythun <sdohri21@wooster.edu>
2019-01-18 17:01:44 -0500

    Solved the python exercism back account problem.

--- exercism/python/bank-account/bank_account.py
+++ exercism/python/bank-account/bank_account.py
@@ -1,18 +1,44 @@
 class BankAccount(object):
     def __init__(self):
-        pass
+        self.balance = 0
+        self.open_account = False
 
     def get_balance(self):
-        pass
+        if self.open_account:
+            return self.balance
+        else:
+            raise ValueError(".+")
 
     def open(self):
-        pass
+        if self.open_account:
+            raise ValueError(".+")
+        else:
+            self.open_account = True
 
     def deposit(self, amount):
-        pass
+        if amount < 0:
+            raise ValueError(".+")
+        if self.open_account:
+            self.balance += amount
+        else:
+            raise ValueError(".+")
+
 
     def withdraw(self, amount):
-        pass
+        if amount < 0:
+            raise ValueError(".+")
+        if self.open_account:
+            if self.balance >= amount:
+                self.balance -= amount
+            else:
+                raise ValueError(".+")
+        else:
+            raise ValueError(".+")
 
     def close(self):
-        pass
+        if self.open_account:
+            self.balance = 0
+            self.open_account = False
+        else:
+            raise ValueError(".+")
+
