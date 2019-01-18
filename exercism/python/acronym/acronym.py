65542e1f7b801043e42d9c49be94ccf7d3603d0f
Cythun <sdohri21@wooster.edu>
2019-01-17 19:57:28 -0500

    Solved the python exercism acronym problem

--- exercism/python/acronym/acronym.py
+++ exercism/python/acronym/acronym.py
@@ -1,2 +1,13 @@
 def abbreviate(words):
-    pass
+    words = words.split(' ')
+    result = ''
+
+    for word in words:
+        for i in range(len(word)):
+            if word[i].isalpha():
+                result += word[i]
+                if '-' in word:
+                    result += word[word.find('-') + 1]
+                break
+
+    return result.upper()
