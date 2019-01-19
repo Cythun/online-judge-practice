e1d4caa5a0ac7b3351ca63aa84e303c3ce3d7c89
Cythun <sdohri21@wooster.edu>
2019-01-19 14:31:15 -0500

    Solved the python exercism protein translation probem.

--- exercism/python/protein-translation/protein_translation.py
+++ exercism/python/protein-translation/protein_translation.py
@@ -1,2 +1,29 @@
 def proteins(strand):
-    pass
+    result = list()
+    def translator(cordon):
+        if cordon == 'AUG':
+            return 'Methionine'
+        elif cordon in ['UUU', 'UUC']:
+            return 'Phenylalanine'
+        elif cordon in ['UUA', 'UUG']:
+            return 'Leucine'
+        elif cordon in ['UCU', 'UCC', 'UCA', 'UCG']:
+            return 'Serine'
+        elif cordon in ['UAU', 'UAC']:
+            return 'Tyrosine'
+        elif cordon in ['UGU', 'UGC']:
+            return 'Cysteine'
+        elif cordon == 'UGG' :
+            return 'Tryptophan'
+        elif cordon in ['UAA', 'UAG', 'UGA']:
+            return 'Stop'
+
+    for i in range(0, len(strand), 3):
+        if i + 3 <= len(strand):
+            translated = translator(strand[i : i + 3])
+            if translated == 'Stop':
+                break
+            else:
+                result.append(translated)
+
+    return result
