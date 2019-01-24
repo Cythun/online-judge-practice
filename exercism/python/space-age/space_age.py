ff0bc7783e886e2960188b7e8d6c30544afd3937
Cythun <sdohri21@wooster.edu>
2019-01-23 22:43:25 -0500

    solved the python exercism space age problem.

--- exercism/python/space-age/space_age.py
+++ exercism/python/space-age/space_age.py
@@ -1,3 +1,21 @@
 class SpaceAge(object):
+
     def __init__(self, seconds):
-        pass
+
+        ratios = list((planet, ratio * 31557600) for planet, ratio in (
+        ('earth', 1.0),
+        ('mercury', 0.2408467),
+        ('venus', 0.61519726),
+        ('mars', 1.8808158),
+        ('jupiter', 11.862615),
+        ('saturn', 29.447498),
+        ('uranus', 84.016846),
+        ('neptune', 164.79132)
+    ))
+
+        self.seconds = seconds
+        for planet, ratio in ratios:
+            setattr(self, 'on_' + planet, self._planet_years(ratio))
+
+    def _planet_years(self, ratio):
+        return lambda ratio=ratio: round(self.seconds / ratio, 2)
